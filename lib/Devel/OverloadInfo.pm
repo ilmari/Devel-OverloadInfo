c25072cf99a4567de87bd0b810d175a15bfe5240
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2014-03-23 18:35:12 +0000

    Add POD

--- lib/Devel/OverloadInfo.pm
+++ lib/Devel/OverloadInfo.pm
@@ -1,5 +1,16 @@
 package Devel::OverloadInfo;
 
+# ABSTRACT: introspect overloaded operators
+
+=head1 DESCRIPTION
+
+Devel::OverloadInfo returns information about L<overloaded|overload>
+operators for a given class (or object), including where in the
+inheritance hierarchy the overloads are declared and where the code
+implementing it is.
+
+=cut
+
 use strict;
 use warnings;
 use overload ();
@@ -21,6 +32,49 @@ sub stash_with_symbol {
     }
     return;
 }
+
+=func overload_info
+
+    my $info = overload_info($class_or_object)
+
+Returns a hash reference with information about all the overloaded
+operators of the argument, which can be either a class name or a blessed
+object. The keys are the overloaded operators, as specified in
+C<%overload::ops> (see L<overload/Overloadable Operations>).
+
+=over
+
+=item class
+
+The name of the class in which the operator overloading was declared.
+
+=item code
+
+A reference to the function implementing the overloaded operator.
+
+=item code_name
+
+The name of the function implementing the overloaded operator, as
+returned by C<sub_fullname> in L<Sub::Identify>.
+
+=item method_name (optional)
+
+The name of the method implementing the overloaded operator, if the
+overloading was specified as a named method, e.g. C<< use overload $op
+=> 'method'; >>.
+
+=item code_class (optional)
+
+The name of the class in which the method specified by C<method_name>
+was found.
+
+=item value (optional)
+
+For the special C<fallback> key, the value it was given in C<class>.
+
+=back
+
+=cut
 
 sub overload_info {
     my $class = blessed($_[0]) || $_[0];
